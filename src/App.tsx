0685546d67c8d0f33f3eeeadf196f9de67de6e53
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-03-16 13:01:12 +0200

    fox1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createElement as e } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return e("div", { className: "container" }, [
+    e("h1", { className: "font-bold", key: 1 }, "Test  JSX!!!"),
+    e(
+      "button",
+      {
+        className: "py-2 px-4 border",
+        key: 2,
+        onClick: () => console.log("Button clicked !!!"),
+      },
+      "Click me !!!"
+    ),
+  ]);
 }
 
 export default App;
